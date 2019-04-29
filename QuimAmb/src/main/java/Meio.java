f4ee0cb7894e45eec8917f5e16b08f034586ffd0
AlannadeAlmeida <45169675+AlannadeAlmeida@users.noreply.github.com>
2019-04-29 17:01:54 -0300

    Update Meio.java

--- QuimAmb/src/main/java/Meio.java
+++ QuimAmb/src/main/java/Meio.java
@@ -3,23 +3,32 @@ import java.util.ArrayList;
 public interface Meio {
 	
 	public void cadatrar (Usuario usuario);
-	  
-	public boolean autentica(int senha);
-    // verifica aqui se a senha confere com a recebida como parametro
+	
+	// verifica aqui se a senha confere com a recebida como parametro  
+	public boolean autentica(String login, String senha);
+    
 	public boolean autentica1(int email);
 	
 	public void salvarusuario (Usuario usuario);
 
 	public void deletarusuario (Usuario usuario);
 	
-	public Usuario getById(Usuario usuario);
-	// A funcao busca e retorna o elemento através de seu identificador
-	public ArrayList<Usuario> getAll();
+	// A funcao busca e retorna o elemento atraves de seu identificador
+	public Usuario getById(Usuario id);
+	
 	// getAll retorna todo o objeto da subclasse armazenada em um banco de dados
+	public ArrayList<Usuario> getAll();
 	
-	public ArrayList<Usuario> acessar();
 	
-	public void login(Usuario usuario);
+	public ArrayList<Eventos> getByCausa(String causa);
+
+	public ArrayList<ElementoQuimico> getByNome(String nome);
+	
+	public ArrayList<MeioAmbiente> getByfloresta(String floresta);
+	
+	public ArrayList<Produto> getBytempodedecomposicao(String tempodedecomposicao);
+	
+	public ArrayList<SeresVivos> getByespecie(String especie);
 	
 	
 }
