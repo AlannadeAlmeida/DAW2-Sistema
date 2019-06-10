8a38778f5847b0daa55e425b804f10e50a0fcd18
Aluno <Aluno@DESKTOP-3FIS8IH>
2019-06-10 10:01:50 -0300

    Thauanny é doida

---
+++ QuimAmb/src/main/java/ElementoQuimicoService.java
@@ -0,0 +1,63 @@
+
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class ElementoQuimicoService implements Serializable, Service<ElementoQuimico> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7803325791425670859L;
+	
+	@Inject
+	private ElementoQuimicoDAO elementoDAO;
+	
+	/* (non-Javadoc)
+	 * @see br.edu.ifpb.esperanca.daw2.services.Service#save(br.edu.ifpb.esperanca.daw2.ifoto.entities.Usuario)
+	 */
+	@Override
+	@TransacionalCdi
+	public void save(ElementoQuimico user) {
+		elementoDAO.save(user);
+	}
+
+	/* (non-Javadoc)
+	 * @see br.edu.ifpb.esperanca.daw2.services.Service#update(br.edu.ifpb.esperanca.daw2.ifoto.entities.Usuario, boolean)
+	 */
+	@Override
+	@TransacionalCdi
+	public void update(ElementoQuimico user)  {
+			elementoDAO.update(user);
+	}
+
+	/* (non-Javadoc)
+	 * @see br.edu.ifpb.esperanca.daw2.services.Service#delete(br.edu.ifpb.esperanca.daw2.ifoto.entities.Usuario)
+	 */
+	@Override
+	@TransacionalCdi
+	public void remove(ElementoQuimico user) {
+		elementoDAO.remove(user);
+	}
+
+	/* (non-Javadoc)
+	 * @see br.edu.ifpb.esperanca.daw2.services.Service#getByID(long)
+	 */
+	@Override
+	public ElementoQuimico getByID(long userId)  {
+			return elementoDAO.getByID(userId);
+	}
+
+	/* (non-Javadoc)
+	 * @see br.edu.ifpb.esperanca.daw2.services.Service#getAll()
+	 */
+	@Override
+	public List<ElementoQuimico> getAll() {
+			return elementoDAO.getAll();
+	}
+		
+}
