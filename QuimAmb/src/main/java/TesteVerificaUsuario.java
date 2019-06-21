4f59f3354640b380fb3291917c4ce2ab85559167
Aluno <Aluno@DESKTOP-3FIS8IH>
2019-06-21 11:59:37 -0300

    TesteVerificaUsuario

---
+++ QuimAmb/src/main/java/TesteVerificaUsuario.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class TesteVerificaUsuario {
+	
+	class Testes {
+
+		@Test
+		void verificarCadastrarUsuario() {
+			UsuarioService service = new UsuarioService();
+			Usuario usuario = new Usuario();
+			usuario.setLogin("login");
+			usuario.setEmail("email");
+			service.save(usuario);
+			assertNotNull(usuario.getId());
+			Usuario outro = service.getByID(usuario.getId());
+			assertEquals(outro, usuario);
+			usuario.setNome("nome");
+			service.update(usuario);
+			outro = service.getByID(usuario.getId());
+			assertEquals(outro.getNome(), usuario.getNome());
+			service.remove(usuario);
+			outro = service.getByID(usuario.getId());
+			assertNull(outro);
+			
+		}
+	}
+}
+
